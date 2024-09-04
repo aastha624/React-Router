28d2108c70f3ccc0a8b3a64e1e44434359823ec4
aastha624 <patelaastha2910@gmail.com>
2024-09-05 00:44:08 +0530

    roterbasic,browserrouter,navigatehook,nestedrouter,index,outlet

--- reactrouter/src/App.js
+++ reactrouter/src/App.js
@@ -1,23 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+ import {NavLink, Route,Routes} from "react-router-dom";
+import './App.css'; 
+import Home from "./components/Home";
+import Support from "./components/Support";
+import About from "./components/About";
+import Labs from "./components/Labs";
+import Notfound from "./components/Notfound";
+import MainHeader from "./components/MainHeader";
+
 
 function App() {
-  return (
+  return ( 
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      {/* <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/support">Support</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/labs">Labs</Link>
+          </li>
+        </ul>
+      </nav> */}
+      
+      <nav>
+        <ul>
+          <li>
+            <NavLink to="/">Home</NavLink>
+          </li>
+          <li>
+            <NavLink to="/support">Support</NavLink>
+          </li>
+          <li>
+            <NavLink to="/about">About</NavLink>
+          </li> 
+          <li>
+            <NavLink to="/labs">Labs</NavLink>
+          </li>
+        </ul>
+      </nav>
+
+        {/* <Route path="/" element={<Home />} />
+        <Route path="/support" element={<Support />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/labs" element={<Labs />} />
+        <Route path="*" element={<NotFound />} /> */}
+
+
+      <Routes>
+         <Route path="/" element={<MainHeader></MainHeader>}>
+<Route index element={<Home></Home>}/>
+         {/* index likhaneka matalab default route */}
+         <Route path="/support" element={<Support></Support>}></Route>
+         <Route path="/about" element={<About></About>}></Route>
+         <Route path="/labs" element={<Labs></Labs>}></Route>
+         <Route path="/*" element={<Notfound></Notfound>}></Route>
+         </Route>
+      </Routes> 
+     
     </div>
   );
 }
